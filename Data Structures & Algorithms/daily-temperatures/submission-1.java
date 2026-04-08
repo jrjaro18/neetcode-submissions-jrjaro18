42d83aa240f896dfd4eea671214112705e8d7e6e
Rohan Jaiswal <101345854+jrjaro18@users.noreply.github.com>
2026-04-08 22:40:44 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        var stack = new Stack<Integer>();
+        var sol = new int[temperatures.length];
+        for (var i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                var curr = stack.pop();
+                sol[curr] = i - curr;
+            }
+            stack.push(i);
+        }
+        return sol;
+    }
+}
