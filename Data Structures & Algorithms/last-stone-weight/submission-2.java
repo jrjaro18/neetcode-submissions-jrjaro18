658872b0dc8ad01f60d0e0454061b1ae1cc1da5e
Rohan Jaiswal <101345854+jrjaro18@users.noreply.github.com>
2026-06-05 03:02:15 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        var arena = new PriorityQueue<Integer>((x, y) -> y - x);
+        for (var stone : stones) arena.offer(stone);
+        while (arena.size() != 1) {
+            var contestant1 = arena.remove();
+            var contestant2 = arena.remove();
+            var winner = Math.abs(contestant1 - contestant2);
+            arena.offer(winner);
+        }
+        return arena.remove();
+    }
+}
